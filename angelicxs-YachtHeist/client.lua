f1861e674cb75aaaed95adf4a1446eaeac9df664
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-01-31 23:45:33 -0700

    ESX = exports["es_extended"]:getSharedObject()

--- angelicxs-YachtHeist/client.lua
+++ angelicxs-YachtHeist/client.lua
@@ -61,14 +61,7 @@ end)
 
 CreateThread(function()
     if Config.UseESX then
-        while ESX == nil do
-            ESX = exports["es_extended"]:getSharedObject()
-            Wait(0)
-        end
-    
-        while not ESX.IsPlayerLoaded() do
-            Wait(100)
-        end
+        ESX = exports["es_extended"]:getSharedObject()
     
         local playerData = ESX.GetPlayerData()
         CreateThread(function()
